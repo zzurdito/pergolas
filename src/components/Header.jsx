5eb629ce20b138efa6b3842e4b2b72e28075af33
zzurdito <128636621+zzurdito@users.noreply.github.com>
2024-10-03 10:07:03 +0200

    cambios 10032024

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,10 +7,10 @@ function Header() {
     <div className="header-pag">
         <Nav />
         <div className="container container-header">
-          <h1 className="title">Pérgolas Canor</h1>
-          <h2 className="">Fabricación propia de pérgolas de madera
+          <h1 className="__title">Pérgolas Canor</h1>
+          <h2 className="__desc">Fabricación propia de pérgolas de madera
           en Murcia y Benissa</h2>
-          <h3 className="">La mejor pérgola al mejor precio.
+          <h3 className="__desc header__desc">La mejor pérgola al mejor precio.
           Dinos tu presupuesto, ¡te lo mejoramos!</h3>
           <BotonPresupuesto />
         </div>
